cbcad2781bd1f8e230f03168ff1c1cc1d5dc85f4
AS-Coope <deltazirconium@gmail.com>
2023-11-05 23:30:49 -0500

    Prevent form from taking action and submitting

--- scripts.js
+++ scripts.js
@@ -1,1 +1,16 @@
 /* Add your JavaScript to this file */
+document.addEventListener("DOMContentLoaded", function () {
+    var formElement = document.querySelector("form");
+    var emailInputField = document.getElementById("email");
+    var emailSubscribeBtn = document.getElementsByClassName("btn")[1];
+    var emailSubscriptionMessage = document.getElementsByClassName("message")[0];
+
+    formElement.addEventListener("submit", (event) => {
+        event.preventDefault(); // prevents form from trying to submit data/ take action
+
+        emailSubscribeBtn.addEventListener('click', (event) => {
+
+        });
+
+    });
+});
