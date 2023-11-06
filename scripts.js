5436a2fda3af884ca322db07e73063c153907235
AS-Coope <deltazirconium@gmail.com>
2023-11-05 23:39:57 -0500

    Perform check to see if a valid email has been submitted

--- scripts.js
+++ scripts.js
@@ -9,7 +9,12 @@ document.addEventListener("DOMContentLoaded", function () {
         event.preventDefault(); // prevents form from trying to submit data/ take action
 
         emailSubscribeBtn.addEventListener('click', (event) => {
-
+            if (emailInputField.value.trim() === "") {
+                emailSubscriptionMessage.textContent = "Please enter a valid email address";
+            } else {
+                emailSubscriptionMessage.textContent = `Thank you! Your email address 
+                ${emailInputField.value.trim()} has been added to our mailing list!`;
+            }
         });
 
     });
